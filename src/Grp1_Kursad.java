23ec931ac2524d8f8c76d1e6e7220ed58abb14fb
kursad <kursadbal89@gmail.com>
2022-04-22 16:45:44 +0200

    third commit

--- src/Grp1_Kursad.java
+++ src/Grp1_Kursad.java
@@ -2,5 +2,6 @@ public class Grp1_Kursad {
     public static void main(String[] args) {
         System.out.println("Login class is created");
         System.out.println("AC1 is completed");
+        System.out.println("AC2 is completed");
     }
 }
